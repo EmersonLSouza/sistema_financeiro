0ac7aed33ddce3ab1d31c2e5884b3af5637891f6
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-09-30 20:48:46 -0300

    Continuação PHP

---
+++ php/conexao.php
@@ -0,0 +1,22 @@
+<?php
+
+// Conectando no Banco de Dados
+
+$servidor = 'localhost';
+$usuario = 'root';
+$senha = 'root';
+$db = 'db_contabilidade';
+
+$conexao = new mysqli($servidor, $usuario, $senha, $db);
+
+if ($conexao ->connect_error){
+    die('falha na conexão'. $conexao -> connect_error);
+}
+else{
+    echo "✔ Conectado com Sucesso!";
+}
+
+
+
+
+?>
