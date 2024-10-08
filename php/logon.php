ca2dd7ff21b3a480b62847101796a93d0eff7ada
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-10-07 21:29:06 -0300

    Banco de dados e PHP

---
+++ php/logon.php
@@ -0,0 +1,33 @@
+<?php
+// Recebendo os dados da tela
+
+$email = $_POST['email'];
+$password = $_POST['senha'];
+
+//link da conexão
+
+include 'conexao.php'
+
+//Pegar os dados no banco
+ 
+$sql = "SELECT * FROM tb_user WHERE email = '$email'";
+
+$query = $conexao->query($sql);
+
+$resultado = $query->fetch_assoc();
+
+print_r($resultado);
+
+$email_banco = $resultado['email'];
+$senha_banco = $resultado['senha'];
+
+
+if($email = $email_banco && $password == $senha_banco){
+    header('location: tela_inicial.html');
+}
+else{
+    echo"<script> alert ('Usuario ou senha invalida'); history.back(); </script>";
+}
+
+
+?>
